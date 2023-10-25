9384a11a15f24dd924fbb1b753e0d8a7b68354ee
Stan108 <kage74@yandex.ru>
2023-10-25 16:19:07 +0300

    second commit in second branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("this is second branch");
+        System.out.println("second message added");
     }
 }
