8dc1055a36f38c304ee8d87c58bf83dd6f03b08f
Stan108 <kage74@yandex.ru>
2023-10-25 16:27:57 +0300

    new commit in master branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("this is second branch");
-        System.out.println("second message added");
+        System.out.println("this is master branch");
     }
 }
